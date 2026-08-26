985346cece76109fbf7720f89067272c264ab6db
Daniel <drametbarea@gmail.com>
2026-08-26 09:43:35 +0200

    Add splash screen, modal notification by image

--- public/app.js
+++ public/app.js
@@ -203,7 +203,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
             row.innerHTML = `
                 <td>${index + 1}</td>
-                <td>${name} (${data.uid})</td>
+                <td>${name}</td>
                 <td>${data.count}</td>
                 <td>${timeStr}</td>
             `
@@ -226,14 +226,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
     }
 
-    // Función para mostrar el modal animado NFC solo con el mensaje aleatorio
+    // Función para mostrar el modal animado NFC
     function showNFCModal() {
         const overlay = document.getElementById('modalOverlay');
         const messageEl = document.querySelector('.modal-message');
+        const image = document.querySelector('.chibi-modal');
 
         // Seleccionar un mensaje aleatorio
         const randomIndex = Math.floor(Math.random() * festiveMessages.length);
         messageEl.textContent = festiveMessages[randomIndex];
+
+        // Añadir efecto de guiño al chibi
+        image.classList.add('wink');
+        setTimeout(() => image.classList.remove('wink'), 500);
 
         // Mostrar modal
         overlay.classList.remove('modal-hidden');
